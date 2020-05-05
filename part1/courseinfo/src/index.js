e73bdd31c13863315dbb4e88ede2f598a4ee12f0
notypicalus <notypicalus@gmail.com>
2020-05-05 13:54:34 +0300

    1.1: course information, step1 Refactor the code so that it consists of three new components: Header, Content, and Total. All data still resides in the App component, which passes the necessary data to each component using props.

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -4,32 +4,32 @@ import ReactDOM from 'react-dom';
 // Refactor the code so it consists of three new components: Header, Content, Total
 
 const Header = (props) => {
-  return (<div><h1>{props.course}</h1></div>)
+  return (<h1>{props.course}</h1>)
+
 }
 
 const Content = (props) => {
-return (<div><h1>{props.number}</h1></div>)
+return (<div><p>{props.parts}</p></div>)
 }
 
-const App = () => {
-  
-  const course = {
-  part1: 'Fundamentals of React',
-  part2: 'Using props to pass data',
-  part3: 'State of a component'
-  }
-  
-  const number = {
-  exercises1: 10,
-  exercises2: 7,
-  exercises3: 14
+const Total = (props) => {
+return (<div><p>{props.count}</p></div>)
 }
+
+const App = (props) => {
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
-    <Header course = {props.course}/>
-    <Content parts= {props.parts} />
-    <Total number = {exercises1 + exercises2 + exercises3}/>
+    <Header course = {course}/> 
+    <Content parts= {part1 + part2 + part3}  />
+    <Total count = {exercises1 + exercises2 + exercises3}/>
     </div>
   )
 }
