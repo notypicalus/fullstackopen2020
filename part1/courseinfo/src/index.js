355ac28cce5cebb83b6dc94e7cf7cffb7ecd5b1e
notypicalus <notypicalus@gmail.com>
2020-05-13 14:45:03 +0300

    1.3: course info, step3

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,31 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Content from './components/Content';
-import Header from './components/Header';
-import Total from './components/Total';
-
-
-// Refactor the code so it consists of three new components: Header, Content, Totals
+import Content from './components/Content'
+import Header from './components/Header'
+import Total from './components/Total'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   const parts = [
-    {name:part1, count:exercises1},
-    {name:part2, count:exercises2},
-    {name:part3, count:exercises3}
-  ]
+    { name: part1, count: exercises1, id: 1 },
+    { name: part2, count: exercises2, id: 2},
+    { name: part3, count: exercises3, id: 3},
+  ];
 
   return (
     <div>
-    <Header course = {course}/> 
-    <Content parts = {parts}/>
+    <Header course = {course}/>  
+    <Content parts = {parts} />
     <Total totalExercise = {exercises1 + exercises2 + exercises3}/>
     </div>
   )
