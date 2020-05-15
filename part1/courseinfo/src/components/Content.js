6cee35660a0541a9852b1aeb38f32ba423c12c45
notypicalus <notypicalus@gmail.com>
2020-05-15 14:15:59 +0300

    1.1: course info, step1 Refactor the code so that it consists of three new components: Header, Content, and Total.

---
+++ part1/courseinfo/src/components/Content.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Content = ({exercises}) => {
+
+return (<p>Number of exercises {exercises}</p>)
+}
+
+
+export default Content;
---
+++ part1/courseinfo/src/components/Total.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Total = ({count}) => {
+return (<p>{count}</p>)     
+}
+
+export default Total
