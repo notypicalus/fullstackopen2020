f5cfd6e6ec15945cf59bc36e7432281f90cb397f
notypicalus <notypicalus@gmail.com>
2020-05-19 12:15:48 +0300

    1.4: course information step4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -7,33 +7,28 @@ import Total from './components/Total'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
-
-
-  const exercise1 = part1.exercises
-  const exercise2 = part2.exercises
-  const exercise3 = part3.exercises
-
-  const exercises = [exercise1, exercise2, exercise3]
-
-
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+  
   return (
     <div>
-      <Header name={part1.name + part2.name + part3.name} course={course}/>
-      <Content exercises={exercises}/>
-      <Total count={part1.exercises + part2.exercises + part3.exercises}/>
-      </div>
+      <Header course={course}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
+      
+    </div>
   )
 }
 
